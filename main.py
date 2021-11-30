2c424f02219ccd5fcdda8d3e044d72726c1a41f1
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-11-30 14:40:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -232,5 +232,5 @@ def delete_post(post_id):
 # if __name__ == "__main__":
 #     app.run(host='0.0.0.0', port=5000)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
